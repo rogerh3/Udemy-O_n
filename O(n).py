1d66a0c93dbc02d61916edab27ed802adf81f464
Roger H Hayden III <81320876+rogerh3@users.noreply.github.com>
2022-01-14 08:36:57 -0500

    Add files via upload

---
+++ O(n).py
@@ -0,0 +1,17 @@
+#O(n) - Passed it the number n and ran n times
+def print_items(n):
+    for i in range(n):
+        print(i)
+
+print_items(10)
+
+           
+              
+                  
+                  
+
+
+
+
+
+
